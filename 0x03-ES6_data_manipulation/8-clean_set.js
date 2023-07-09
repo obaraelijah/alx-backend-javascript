cb039c59ebef339d48351fc83fb8aa55965518fa
elijah obara <elijahobara357@gmail.com>
2023-07-09 14:38:00 +0300

    Hot fix

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -2,7 +2,7 @@ export default function cleanSet(set, startString) {
   let filteredArr;
 
   if (startString !== '') {
-    filteredArr = [...set].filter((str) => str.startsWith(startString));
+    filteredArr = Array.from(set).filter((str) => str.startsWith(startString));
     filteredArr = filteredArr.map((str) => str.slice(startString.length));
   } else {
     filteredArr = [];
