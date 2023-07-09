e1d89f3b2a1c2dafdc4cb1479d4fe6beb022d60b
elijah obara <elijahobara357@gmail.com>
2023-07-09 11:19:13 +0300

    chore: hot fix

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,13 +1,12 @@
 export default class Building {
-    constructor(sqft) {
-      this._sqft = sqft;
+  constructor(sqft) {
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
-  
-    get sqft() {
-      return this._sqft;
-    }
-  
-    evacuationWarningMessage() {
-      throw new Error('Class extending Building must override evacuationWarningMessage');
-    }	
+    this._sqft = sqft;
   }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
