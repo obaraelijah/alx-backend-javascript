9e897852f5ae8c670f263c013919e1169282b6a8
elijah obara <elijahobara357@gmail.com>
2023-07-09 11:12:38 +0300

    chore: static methods and methods

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,30 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amt) {
+    this._amount = amt;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(curr) {
+    this._currency = curr;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
