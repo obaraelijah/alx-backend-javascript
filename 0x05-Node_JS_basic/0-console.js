ce909ad457efb07af5a299a918261d45fdd162a6
obaraelijah <elijahobara357@gmail.com>
2023-08-22 10:14:08 +0300

    Executing basic javascript with Node JS

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,5 +1,9 @@
-function displayMessage(message) {
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} message The message to display.
+ */
+const displayMessage = (message) => {
   console.log(message);
-}
+};
 
 module.exports = displayMessage;
